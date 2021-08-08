2db0320de0c69bcb83148d74f711e8e9b1729ff1
JunsuLime <vfv0121@gmail.com>
2021-08-08 23:45:36 +0900

    [#2] 어노테이션 기반 설정 값 관리

--- src/main/java/junsulime/cloud/CloudApplication.java
+++ src/main/java/junsulime/cloud/CloudApplication.java
@@ -1,15 +1,58 @@
 package junsulime.cloud;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
-public class CloudApplication {
+import javax.annotation.PostConstruct;
+
+@PropertySource("some.properties")
+@Configuration
+public class CloudApplication implements EnvironmentAware {
 
     public static void main(String[] args) {
-        new ClassPathXmlApplicationContext("classic.xml");
+        new AnnotationConfigApplicationContext(CloudApplication.class);
     }
 
-    public void setConfigurationProjectName(String projectName) {
-        System.out.println("Project name is " + projectName);
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Value("${configuration.projectName}")
+    private String fieldName;
+
+    public CloudApplication(@Value("${configuration.projectName}") String fieldName) {
+        System.out.println("constructor: " + fieldName);
+    }
+
+    @Value("${configuration.projectName}")
+    public void setProjectName(String fieldName) {
+        System.out.println("setter: " + fieldName);
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        System.out.println("environment: " + environment.getProperty("configuration.projectName"));
+    }
+
+    @Bean
+    InitializingBean both(Environment environment, @Value("${configuration.projectName}") String projectName) {
+        return () -> {
+            System.out.println("initializingBean(env): " + environment.getProperty("configuration.projectName"));
+            System.out.println("initializingBean(property): " + projectName);
+        };
+    }
+
+    @PostConstruct
+    public void onInit() {
+        System.out.println("fieldName: " + fieldName);
     }
 
 }
