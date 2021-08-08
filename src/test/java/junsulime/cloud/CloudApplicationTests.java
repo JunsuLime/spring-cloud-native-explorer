284659419da5fc74223d5d6d39058b2d70d71b63
JunsuLime <vfv0121@gmail.com>
2021-08-08 22:01:00 +0900

    [#1] 프로젝트 초기화

---
+++ src/test/java/junsulime/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package junsulime.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
