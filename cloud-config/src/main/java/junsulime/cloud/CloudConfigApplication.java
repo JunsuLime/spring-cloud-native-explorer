0174825476256de6192d1b3ef13ef3294640c00d
JunsuLime <vfv0121@gmail.com>
2021-08-16 20:32:25 +0900

    [#6] CloudConfig 서버 의존성 설정하기

--- cloud-config/src/main/java/junsulime/cloud/CloudConfigApplication.java
+++ cloud-config/src/main/java/junsulime/cloud/CloudConfigApplication.java
@@ -1,8 +1,14 @@
 package junsulime.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
 public class CloudConfigApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Subproject");
+        SpringApplication.run(CloudConfigApplication.class);
     }
 }
