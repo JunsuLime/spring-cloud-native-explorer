2eef224b3504fc9878525d1a3928649d8f75376c
Lim jun su <vfv0121@gmail.com>
2021-08-21 05:54:39 +0900

    [#10] 새로고침 가능한 설정 (#12)
    * [#10] RefreshScope 을 이용하여 설정 갱신하기
    
    * [#10] rabbitmq 를 docker 로 실행시키자
    
    * [#10] 클라이언트를 이벤트 버스에 연결시키자

---
+++ src/main/java/junsulime/cloud/ProjectNameRestController.java
@@ -0,0 +1,23 @@
+package junsulime.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ProjectNameRestController {
+
+    private final String projectName;
+
+    public ProjectNameRestController(@Value("${configuration.projectName}") String projectName) {
+        System.out.println("ProjectNameRestController constructed");
+        this.projectName = projectName;
+    }
+
+    @GetMapping("/project-name")
+    public String getProjectName() {
+        return projectName;
+    }
+}
---
+++ src/main/java/junsulime/cloud/RefreshCounter.java
@@ -0,0 +1,22 @@
+package junsulime.cloud;
+
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Component
+public class RefreshCounter {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    public RefreshCounter() {
+        System.out.println("RefreshCounter constructed");
+    }
+
+    @EventListener
+    public void refresh(RefreshScopeRefreshedEvent e) {
+        System.out.println("count: " + counter.incrementAndGet());
+    }
+}
