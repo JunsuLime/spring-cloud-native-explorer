160dc53f50d7e9121f8b1dae5e4db34389209270
Lim jun su <vfv0121@gmail.com>
2021-08-22 19:15:30 +0900

    [#11] 설정정보 변경을 감지해 클라이언트에게 푸시 메시지를 보내준다. (#15)
    * [#11] config server 에 모니터 및 버스 의존성 추가하기
    
    * [#11] rabbitmq virtualhost 설정 추가하기

---
+++ cloud-config/src/main/java/junsulime/cloud/WebConfiguration.java
@@ -0,0 +1,14 @@
+package junsulime.cloud;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingHandlerInterceptor());
+    }
+}
