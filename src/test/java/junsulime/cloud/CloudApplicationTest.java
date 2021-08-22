ca7e290e3b068272d41c2a1f02e0d06b27202d56
JunsuLime <vfv0121@gmail.com>
2021-08-22 23:43:34 +0900

    [#17] 스프링부트 어플리케이션 테스트
    application context 정상 load 를 테스트하였다.

---
+++ src/test/java/junsulime/cloud/CloudApplicationTest.java
@@ -0,0 +1,13 @@
+package junsulime.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
