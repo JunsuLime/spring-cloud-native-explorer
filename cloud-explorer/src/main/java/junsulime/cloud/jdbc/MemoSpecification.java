879bd11615935b65169642c82882f8e0696e7867
Lim jun su <vfv0121@gmail.com>
2021-11-14 02:42:08 +0900

    [#31] jdbcTemplate 사용 및 테스트코드 작성 (#39)

---
+++ cloud-explorer/src/main/java/junsulime/cloud/jdbc/MemoSpecification.java
@@ -0,0 +1,14 @@
+package junsulime.cloud.jdbc;
+
+public class MemoSpecification {
+
+    private final String title;
+
+    public MemoSpecification(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
