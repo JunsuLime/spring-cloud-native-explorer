e7c4dbaf85a8529812d03790de06cc452890568f
JunsuLime <vfv0121@gmail.com>
2022-01-09 22:23:25 +0900

    [#40] 타임아웃 혹은 잠시 downtime 이슈의 경우 재시도를 하자

--- cloud-explorer/src/main/java/junsulime/cloud/openfeign/SelfCallerConfiguration.java
+++ cloud-explorer/src/main/java/junsulime/cloud/openfeign/SelfCallerConfiguration.java
@@ -1,10 +1,43 @@
 package junsulime.cloud.openfeign;
 
-import feign.Logger;
+import feign.*;
 import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 
 public class SelfCallerConfiguration {
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new ErrorDecoder() {
+            @Override
+            public Exception decode(String methodKey, Response response) {
+                final FeignException feignException = FeignException.errorStatus(methodKey, response);
+                if (isRetryable(feignException)) {
+                    return new RetryableException(
+                            feignException.status(),
+                            feignException.getMessage(),
+                            feignException.request().httpMethod(),
+                            feignException,
+                            null,
+                            feignException.request()
+                    );
+                }
+                return feignException;
+            }
+
+            private boolean isRetryable(FeignException exception) {
+                return exception instanceof FeignException.GatewayTimeout
+                        || exception instanceof FeignException.BadGateway
+                        || exception instanceof FeignException.ServiceUnavailable;
+            }
+        };
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(500, 1000, 3);
+    }
 
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
